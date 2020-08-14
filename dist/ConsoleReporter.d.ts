bb0119b405c49b18493151c192c4e47b6a6f9ffd
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-08-14 14:19:27 +0530

    `middleware` exported as declaration, `FoxxReporter` no longer re-exported.

--- dist/ConsoleReporter.d.ts
+++ dist/ConsoleReporter.d.ts
@@ -1,18 +1,19 @@
 import { SpanData } from '@recallgraph/foxx-tracer';
-
 declare const _default: {
-  new(namespace?: string): {
-    report(traces: SpanData[][]): void;
-    readonly _config: any;
-    readonly config: any;
-  };
-  readonly FORMAT_OPTIONS: Readonly<{
-    depth: number;
-    maxArrayLength: number;
-    breakLength: number;
-    compact: boolean;
-    sorted: boolean;
-  }>;
+    new (namespace?: string): {
+        report(traces: SpanData[][]): void;
+        readonly _config: any;
+        readonly config: {
+            [key: string]: any;
+        };
+    };
+    readonly FORMAT_OPTIONS: Readonly<{
+        depth: number;
+        maxArrayLength: number;
+        breakLength: number;
+        compact: boolean;
+        sorted: boolean;
+    }>;
 };
 export = _default;
 //# sourceMappingURL=ConsoleReporter.d.ts.map
--- dist/ConsoleReporter.js
+++ dist/ConsoleReporter.js
@@ -1,35 +1,34 @@
-'use strict'
-var _a
-const foxx_tracer_1 = require('@recallgraph/foxx-tracer')
-const util_1 = require('util')
+"use strict";
+var _a;
+const foxx_tracer_1 = require("@recallgraph/foxx-tracer");
+const util_1 = require("util");
 module.exports = (_a = class ConsoleReporter extends foxx_tracer_1.reporters.Reporter {
-  constructor (namespace = 'console') {
-    super(namespace)
-  }
-
-  report (traces) {
-    const { stream } = this.config
-    let out
-    switch (stream) {
-      case 'log':
-        out = console.log.bind(console)
-        break
-      case 'error':
-        out = console.error.bind(console)
-        break
-      case 'debug':
-      default:
-        out = console.debug.bind(console)
-    }
-    out(util_1.inspect(traces, ConsoleReporter.FORMAT_OPTIONS))
-  }
-},
-  _a.FORMAT_OPTIONS = Object.freeze({
-    depth: Infinity,
-    maxArrayLength: Infinity,
-    breakLength: Infinity,
-    compact: true,
-    sorted: true
-  }),
-  _a)
+        constructor(namespace = 'console') {
+            super(namespace);
+        }
+        report(traces) {
+            const { stream } = this.config;
+            let out;
+            switch (stream) {
+                case 'log':
+                    out = console.log.bind(console);
+                    break;
+                case 'error':
+                    out = console.error.bind(console);
+                    break;
+                case 'debug':
+                default:
+                    out = console.debug.bind(console);
+            }
+            out(util_1.inspect(traces, ConsoleReporter.FORMAT_OPTIONS));
+        }
+    },
+    _a.FORMAT_OPTIONS = Object.freeze({
+        depth: Infinity,
+        maxArrayLength: Infinity,
+        breakLength: Infinity,
+        compact: true,
+        sorted: true
+    }),
+    _a);
 //# sourceMappingURL=ConsoleReporter.js.map
